bdaa2a02391529e5d07fbbd3290f8083e8c771ac
Amritpal singh <amritpal15k@gmail.com>
2026-03-09 17:21:44 -0700

    delete again

--- palindrome.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(word) {
-  const cleanWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
-  const reversed = cleanWord.split('').reverse().join('');
-  return cleanWord === reversed;
-}
-
-module.exports = isPalindrome;
